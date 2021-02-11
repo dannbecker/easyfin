9137ea9aae7d886b209c52abc70eed23ddadf2f5
arthurrivas <arthurrivas1@gmail.com>
2021-02-11 17:51:34 -0300

    add npm packages
    Adicionei pacotes do npm que

--- Js/index.js
+++ Js/index.js
@@ -1,7 +1,7 @@
-const customExpress = require('C:/Users/Ale/Desktop/easyfin/Js/customExpress.js')
+const customExpress = require('C:/Users/Ale/Documents/GitHub/easyfin/Js/customExpress.js')
 const app = customExpress()
-const conexao = require('C:/Users/Ale/Desktop/easyfin/Js/conexao')
-const TabelaAlunos = require('C:/Users/Ale/Desktop/easyfin/Js/TabelaAlunos.js');
+const conexao = require('C:/Users/Ale/Documents/GitHub/easyfin/Js/conexao')
+const TabelaAlunos = require('C:/Users/Ale/Documents/GitHub/easyfin/Js/TabelaAlunos.js');
 
 conexao.connect((erro, acerto) => {
 
